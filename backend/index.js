4ae6235944affcbf4350ac89bf77deae5c1b949a
Stipo Margić <stipo@udruga-liberato.hr>
2022-12-22 15:29:58 +0100

    Connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
+const { connection } = require("./config/db");
+const bcrypt = require("bcrypt");
+const { signJwt, verifyJwt } = require("./config/jwt");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -9,7 +12,17 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.listen(port, () => {
 	console.log("Running on port " + port);
 });
+
+
+const init = async () => {
+	try {
+		await connection.authenticate();
+		console.log("Connection has been established successfully.");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
+};
+init();
