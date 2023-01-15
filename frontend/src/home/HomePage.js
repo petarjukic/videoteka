ac5b45c405bec94d4fe876dc508ade8b767214c0
Stipo Margić <stipo@udruga-liberato.hr>
2023-01-15 11:34:09 +0100

    Sort by year

--- frontend/src/home/HomePage.js
+++ frontend/src/home/HomePage.js
@@ -3,13 +3,22 @@ import { Link } from "react-router-dom";
 
 const HomePage = ({ searchTerm }) => {
   const [movies, setMovies] = useState([]);
-const [newMovies, setNewMovies] = useState([])
+  const [newMovies, setNewMovies] = useState([])
+  const [genres, setGenres] = useState([]);
+  const [dropdown, setDropdown] = useState("");
   useEffect(() => {
     const fetchMovies = async () => {
       const response = await fetch("http://localhost:4000/api/movies");
       const data = await response.json();
       setMovies(data);
     };
+
+    const fetchGenres = async () => {
+      const response = await fetch("http://localhost:4000/api/genres");
+      const data = await response.json();
+      setGenres(data);
+    };
+    fetchGenres();
     fetchMovies();
   }, []);
 
@@ -25,9 +34,30 @@ const [newMovies, setNewMovies] = useState([])
     setNewMovies(newMovies);
   }, [movies, searchTerm]);
 
+  useEffect(() => {
+    if (dropdown === "Asc") {
+      const newMovies = movies.sort((a, b) => {
+        return a.year - b.year;
+      });
+      setNewMovies(newMovies);
+    } else {
+      const newMovies = movies.sort((a, b) => {
+        return b.year - a.year;
+      });
+      setNewMovies(newMovies);
+    }
+  }, [dropdown, movies]);
+
   return (
     <div id="shell">
-        <div id="container">
+        <div id="container d-flex">
+        <select value={dropdown} onChange={(e) => setDropdown(e.target.value)}>
+          <option>Sort by year</option>
+            <option value="Asc">
+ASC
+            </option>
+            <option value="Desc">DESC</option>
+        </select>
           <div className="row w-100">
           {newMovies.map((movie) => (
 <div className="col-2">
